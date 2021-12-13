8f23bf0bb1a6ccb23532d3a18f25ed258f375d74
aniketjha1729 <kumaraniket1729@gmail.com>
2021-12-13 18:58:17 +0530

    wip

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,9 @@ import { useSelector } from "react-redux";
 const Home = () => {
   const { user } = useSelector((state) => state);
 
-  return <div style={{ marginTop: "100px" }}>{user.user?.name}</div>;
+  return <div style={{ marginTop: "100px" }}>{user.user?.name}
+  
+  </div>;
 };
 
 export default Home;
